a25c3699b52e5a19ff527d4086e9f2b6f7a7fb7d
Vitor Calado <vitor.santos.calado@gmail.com>
2022-07-01 18:30:10 +0100

    Sec 9

---
+++ GeekShopping/GeekShopping.Web/Startup.cs
@@ -0,0 +1,50 @@
+﻿using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Builder;
+
+namespace GeekShopping.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient<IProductService, ProductService>(c =>
+                    c.BaseAddress = new Uri(Configuration["ServiveUrls:ProductAPI"])
+                );
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
